c7b3d12386171d3719c6c5e44be8ec9de21dda5c
kezhera <kezherashvilinika@gmail.com>
2023-02-16 01:49:39 +0400

    Revert "feat: add react query"
    This reverts commit f9f502230f409b4e756dad8c8bdadc4e13871943.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import Home from "./pages/home";
-
-const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>
+    <Home />
   );
 }
 
